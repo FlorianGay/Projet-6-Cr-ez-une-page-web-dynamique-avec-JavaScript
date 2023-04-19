1de7adb6890bc1d60096106fb71b088d3f9f3cd4
Florian Gay <floriangay750@gmail.com>
2023-04-19 15:28:39 +0200

    Avancement sur la 1ère étape du projet (ajout fonction de création de la gallery et des filtres)

--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,35 +1,64 @@
-/*let works = window.localStorage.getItem('works');
-if (works === null) {
-    //récupération des données
-    const response = await fetch('http://localhost:5678/api/works');
-    works = await response.json();
-    //transformation des données en JSON
-    const valeurWorks = JSON.stringify(works);
-    //stockage dans le localStorage
-    window.localStorage.setItem('works', valeurWorks);
-} else {
-    works = JSON.parse(works);
-}*/
+let works = [];
+let categories = [];
+const gallery = document.querySelector('.gallery');
+const filters = document.querySelector('.filters');
 
-/*let works = [];
+async function fetchCategories() {
+    try {
+        const response = await fetch('http://localhost:5678/api/categories');
+        const listCategories = await response.json();
+        categories = listCategories;
 
-function getData() {
-    fetch('http://localhost:5678/api/works')
-        .then(function (response) {
-            return response.json();
+        listCategories.map((category) => {
+            const filter = document.createElement('li');
+            filter.className = 'filter';
+            filter.dataset.id = category.id;
+            filter.textContent = category.name;
+            filters.appendChild(filter);
         })
-        .then(function (data) {
-            works = data;
-        });
-        
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-getData();
+async function fetchWorks() {
+    try {
+        const response = await fetch('http://localhost:5678/api/works');
+        const listWorks = await response.json();
+        works = listWorks;
+        displayWorks(works)
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-console.log(works);*/
+async function worksFilteredByCategory() {
+    const filterItems = document.querySelectorAll('.filters li');
+    filterItems.forEach((item) => item.addEventListener('click', () => {
+        let worksFiltered = [];
+        gallery.innerHTML = '';
+        // Uniquement si on clique sur un filtre autre que "TOUS"
+        worksFiltered = works.filter(work => work.category.id == item.dataset.id);
+        displayWorks(worksFiltered)
+    }))
+}
 
-fetch('http://localhost:5678/api/works')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+
+addEventListener("DOMContentLoaded", async (event) => {
+    await fetchCategories()
+    await fetchWorks();
+    await worksFilteredByCategory();
+});
+
+
+function displayWorks(works) {
+    works.map((work) => {
+        const workItem = document.createElement('figure');
+        workItem.dataset.id = work.category.id;
+        workItem.innerHTML = `<img src=${work.imageUrl} alt=${work.title}
+            <figcaption>${work.title}</figcaption>`
+        gallery.appendChild(workItem);
+    })
+}
 
 
