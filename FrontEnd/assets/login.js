63837ff14ce4f2a1a31bc485ee97feb45a8c6691
Florian Gay <floriangay750@gmail.com>
2023-04-24 15:17:01 +0200

    Début de la dynamisation de la page de login avec JS

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,12 +1,48 @@
-fetch('http://localhost:5678/api/users/login', {
-    method: 'POST',
-    headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-    },
-    body : JSON.stringify({
-        email: email.value,
-        password: password.value,
-    }),
-})
-    .then((response) => response.json())
+// stockage du token d'identification
+let tokenId = window.localStorage.getItem('tokenId');
+if (tokenId === null) {
+    const valeurToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4';
+    window.localStorage.setItem('tokenId', valeurToken)
+}
+console.log(tokenId);
+
+// Compte et mdp de connexion
+let admin = {
+    email : 'sophie.bluel@test.tld',
+    password: 'S0phie'
+}
+console.log(admin);
+const loginButton = document.getElementById('login-submit');
+
+// Envoie sur l'API
+async function fetchLogin() {
+    await fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: mail,
+            password : password,
+        }),
+    })
+    .then ((response) => response.json())
+}
+
+// Bouton de validation du login
+ loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const mail = document.getElementById('mail').value;
+    const password = document.getElementById('password').value;
+
+    if (mail === admin.email && password === admin.password) {
+        alert('Vous êtes bien connecté(e)');
+        //window.location.href='./index.html';
+    } else {
+        alert('Identifiant out mot de passe incorrect')
+    }
+});
+
+
+
